e1ffe0233a90ced5da84a8b8e9dadd30a07cbb61
Misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2018-10-16 07:03:29 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
